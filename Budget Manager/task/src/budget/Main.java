14cff096d110954ff92261c70e68999016ff81e2
Nick Bauters (7dbb) <nick.bauters@colruytgroup.com>
2024-10-24 10:56:05 +0200

    feat: Parse a shopping list and calculate the total price

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -1,7 +1,23 @@
 package budget;
 
+import budget.util.Keyboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        List<String> list = new ArrayList<>();
+
+        while(Keyboard.hasNextLine()) {
+            String unparsedString = Keyboard.requestNextProvidedLine();
+            list.add(unparsedString);
+        }
+
+        list.stream().forEach(System.out::println);
+        double sum = list.stream()
+                .mapToDouble(s -> Double.parseDouble(s.split("[$]")[1]))
+                .sum();
+        System.out.printf("%nTotal: $%.2f%n", sum);
     }
 }

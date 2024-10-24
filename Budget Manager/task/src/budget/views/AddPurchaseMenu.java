bf95178346b8ce54989e6cefa09390b4da6f0a93
Nick Bauters (7dbb) <nick.bauters@colruytgroup.com>
2024-10-24 14:02:01 +0200

    feat: Add Category choice when a new purchase is registered

--- Budget Manager/task/src/budget/views/AddPurchaseMenu.java
+++ Budget Manager/task/src/budget/views/AddPurchaseMenu.java
@@ -1,5 +1,6 @@
 package budget.views;
 
+import budget.bookkeeping.TransactionCategory;
 import budget.bookkeeping.TransactionManager;
 import budget.util.Keyboard;
 
@@ -10,11 +11,26 @@ public final class AddPurchaseMenu {
     }
 
     public void show() {
+        int categoryChoice = Keyboard.requestNextInt(showCategoryMenu());
         String purchaseName = Keyboard.requestInput("\nEnter purchase name:");
         double purchasePrice = Keyboard.requestNextDouble("Enter its price:");
 
-        manager.registerPurchase(purchaseName, purchasePrice);
+        if (categoryChoice >= TransactionCategory.values().length + 1) {
+            return;
+        }
+
+        manager.registerPurchase(purchaseName, purchasePrice, TransactionCategory.values()[categoryChoice - 1]);
 
         System.out.println("Purchase was added!\n");
+    }
+
+    private String showCategoryMenu() {
+        StringBuilder builder = new StringBuilder("\nChoose the type of purchase\n");
+        int i = 1;
+        for (TransactionCategory cat : TransactionCategory.values()) {
+            builder.append("%d) %s\n".formatted(i++, cat.represent()));
+        }
+        builder.append("%d) Back".formatted(i));
+        return builder.toString();
     }
 }
